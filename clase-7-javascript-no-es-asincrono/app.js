092007316210bed305e903fb2554a70a63b85bb0
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-03-21 16:39:35 -0500

    Javascript no es asincrono

---
+++ clase-7-javascript-no-es-asincrono/app.js
@@ -0,0 +1,12 @@
+function printInConsole()
+{
+    for (var i = 0; i <= 8000; i++)
+    {
+        console.log(i);
+    }
+}
+function clickButton()
+{
+    console.log("Click in button");
+}
+printInConsole();
