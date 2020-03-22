f1d415f242165415598fc89ad4f6647585605d00
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-03-21 20:14:45 -0500

    Escritura dinámica - Tipos Primitivos

---
+++ clase-9-escritura-dinámica-tipos-primitivos/app.js
@@ -0,0 +1,20 @@
+
+var num = 10;
+var str = "Cadena de textos";
+var bol = true;
+var und = undefined;
+var nul = null;
+
+console.log("num =>", num);
+console.log("str =>", str);
+console.log("bol =>", bol);
+console.log("und =>", und);
+console.log("nul =>", nul);
+
+console.warn("----------------------------------");
+
+num = true;
+console.log("num => true", num);
+
+bol = 10;
+console.log("bol => 10", bol);
