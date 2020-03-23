ac9175124f79fdef773b11600b3b2294b6bac584
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-03-22 19:15:40 -0500

    Clase 15 Funciones de primera clase

---
+++ clase-15-funciones-de-primera-clase/app.js
@@ -0,0 +1,16 @@
+
+function person()
+{
+    return {
+        name: "Susana Medina",
+        age: 18,
+        address: {
+            name: "Edificio principal",
+            floor: "8vo"
+        }
+    }
+}
+
+person.zipcode = 11101;
+
+console.log(person());
